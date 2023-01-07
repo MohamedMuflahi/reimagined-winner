fa30c542067fb6cb05b3f43263f4c75f124071cb
Mohamed Muflahi <muflahimohamed88@gmail.com>
2023-01-06 21:12:22 -0500

    added timesheet routes

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -1,2 +1,17 @@
 class TimesheetsController < ApplicationController
+    def index
+        timesheets = Timesheet.all
+        render json: timesheets
+    end
+
+    def create
+        timesheet = Timesheet.create!(user_params)
+        render json: timesheet
+    end
+
+    private 
+    
+    def timesheet_params
+        params.require(:timesheet).permit(:first_name, :last_name, :client, :project, :project_code, :hours, :billable, :billing_rate, :date)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :timesheets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/timesheets", to: "timesheets#index"
+  post "/timesheets", to: "timesheets#create"
+  
 end
