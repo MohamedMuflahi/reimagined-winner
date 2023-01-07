d60f87fc980ddbb7b3f6c73d2d488a4eb9705acb
Mohamed Muflahi <muflahimohamed88@gmail.com>
2023-01-07 00:20:59 -0500

    added missing little features

--- client/src/components/Row.jsx
+++ client/src/components/Row.jsx
@@ -11,17 +11,21 @@ const Row = ({ timesheet }) => {
     const billableAmount = Number((billing_rate * hours).toPrecision(5))
     return (
         <div className="row-container">
-            <div className="row-item">
+            <div className="row-item selectable">
                 <p>{first_name} {last_name}</p>
             </div>
-            <div className="row-item">
+            <div className="row-item selectable">
                 <p>{client}</p>
             </div>
             <div className="row-item right">
                 <p>{hours}</p>
             </div>
             <div className="row-item right">
-                <p>{billable ? hours : 0}</p>
+                <p>{billable ? hours : 0.00}
+                    <span style={{ color: "#ababab" }}>
+                        {billable ? " (100%)" : " (0%)"}
+                    </span>
+                </p>
             </div>
             <div className="row-item right">
                 <h4>{billable ? `$ ${billableAmount}` : "-"}</h4>
