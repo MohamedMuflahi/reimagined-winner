fcd31967d51d7cb251ccce6e9762e60cd921f8cb
Mohamed Muflahi <muflahimohamed88@gmail.com>
2023-01-09 11:27:39 -0500

    refactor with formdata api

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -4,45 +4,31 @@ import "../styles/Form.css"
 const Form = () => {
     const [isFormOpen, setIsFormOpen] = useState(false)
     const [billable, setBillable] = useState(false)
-    const [formData, setFormData] = useState({
-        first_name: "",
-        last_name: "",
-        client: "",
-        project: "",
-        project_code: "",
-        hours: 0,
-        billing_rate: 0,
-        date: ""
-    })
-    const handleFormOpen = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-    }
-    const postNewTimesheet = async (e) => {
+
+    const postNewTimesheet = async (formData) => {
         const request = await fetch("/api/timesheets", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                first_name: formData.first_name,
-                last_name: formData.last_name,
-                client: formData.client,
-                project: formData.project,
-                project_code: formData.project_code,
-                hours: parseInt(formData.hours),
+                first_name: formData.get("first_name"),
+                last_name: formData.get("last_name"),
+                client: formData.get("client"),
+                project: formData.get("project"),
+                project_code: formData.get("project_code"),
+                hours: parseInt(formData.get("hours")),
                 billable: billable,
-                billing_rate: parseInt(formData.billing_rate),
-                date: formData.date
+                billing_rate: parseInt(formData.get("billing_rate")),
+                date:formData.get("date")
             }),
         })
         // Normally would update state here but state is dependent on the page number so I wouldn't want to add it to the wrong page
     }
     const handleFormSubmit = (e) => {
         e.preventDefault()
-        postNewTimesheet()
+        const formData = new FormData(e.target);
+        postNewTimesheet(formData)
     }
     const toggleForm = () => {
         setIsFormOpen(!isFormOpen)
@@ -55,20 +41,20 @@ const Form = () => {
         <div className="form-container">
             {isFormOpen ?
                 <div className="form-container">
-                    <form className="form" onChange={handleFormOpen} onSubmit={handleFormSubmit}>
-                        <label>First Name</label>
-                        <input type="text" name="first_name" id="first_name" />
-                        <label>Last Name</label>
-                        <input type="text" name="last_name" id="last_name" />
-                        <label>Client</label>
-                        <input type="text" name="client" id="client" />
-                        <label>Project</label>
-                        <input type="text" name="project" id="project" />
-                        <label>Project Code</label>
-                        <input type="text" name="project_code" id="project_code" />
-                        <label>Hours</label>
-                        <input type="number" name="hours" id="hours" />
-                        <label >Billable?</label>
+                    <form className="form" onSubmit={handleFormSubmit}>
+                        <label className="input-label">First Name</label>
+                        <input className="input" type="text" name="first_name" id="first_name" />
+                        <label className="input-label">Last Name</label>
+                        <input className="input" type="text" name="last_name" id="last_name" />
+                        <label className="input-label">Client</label>
+                        <input className="input" type="text" name="client" id="client" />
+                        <label className="input-label">Project</label>
+                        <input className="input" type="text" name="project" id="project" />
+                        <label className="input-label">Project Code</label>
+                        <input className="input" type="text" name="project_code" id="project_code" />
+                        <label className="input-label">Hours</label>
+                        <input className="input" type="number" name="hours" id="hours" />
+                        <label className="input-label">Billable?</label>
                         {billable ? <button
                             onClick={handleBilliableButton}>
                             YES
@@ -77,11 +63,10 @@ const Form = () => {
                             <button onClick={handleBilliableButton}>
                                 NO
                             </button>}
-
-                        <label >Billing Rate</label>
-                        <input type="number" name="billing_rate" id="billing_rate" />
-                        <label >Date</label>
-                        <input type="text" name="date" id="date" />
+                        <label className="input-label"> Billing Rate</label>
+                        <input className="input" type="number" name="billing_rate" id="billing_rate" />
+                        <label className="input-label">Date</label>
+                        <input className="input" type="text" name="date" id="date" />
                         <button>
                             Submit
                         </button>
@@ -90,7 +75,7 @@ const Form = () => {
                 </div>
 
                 :
-                <button onClick={toggleForm}>
+                <button className="open-from-button" onClick={toggleForm}>
                     Add Timesheet
                 </button>}
         </div>
